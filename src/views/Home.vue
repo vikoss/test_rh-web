234617ab564ad2a67fb0bf016f0e6cb3020d336d
vikoss <vikoss.yiss@gmail.com>
2022-02-21 21:18:55 -0600

    add mockup to login view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
+  <main>
+    <header-base />
+    <input-base
+      id="email"
+      v-model="email"
+      class="mb-3.5"
+      label="Usuario:"
+      placeholder="Ingresa tu usuario"
+    />
+    <button-base
+      class="max-w-xs mx-auto"
+      :loading="true"
+      :disabled="true"
+      label="Ingresar"
+      @click="login"
+    />
 
+  </main>
 </template>
 
 <script>
+import InputBase from '../components/InputBase.vue';
+import HeaderBase from '../components/HeaderBase.vue';
+import ButtonBase from '../components/ButtonBase.vue';
 
 export default {
   name: 'Home',
   components: {
+    InputBase,
+    HeaderBase,
+    ButtonBase,
+  },
+  data() {
+    return {
+      email: 0,
+    };
+  },
+  methods: {
+    login() {
+      console.log('helooo');
+    },
   },
 };
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,86 @@
 <template>
-
+  <main class="flex flex-wrap min-h-screen"
+  >
+    <section
+      class="bg-gradient-to-b from-blue-200 to-blue-700
+        flex-grow px-6 pt-12 pb-4 sm:pt-9 sm:px-12"
+      style="flex-basis: 540px"
+    >
+      <h1 class="text-white font-medium text-4xl mt-5 mb-1.5 sm:text-6xl lg:absolute sm:bottom-32">
+        Bienvenido
+        <br> a
+        <strong class="text-orange font-bold">
+          RH
+        </strong>
+      </h1>
+      <h2 class="text-white text-base sm:text-xl lg:absolute sm:bottom-24">
+        Sistema de gestión de
+        <strong class="font-bold">recursos humanos.</strong>
+      </h2>
+    </section>
+    <section
+      class="py-7 px-6 sm:px-16 sm:pt-36 flex-grow"
+      style="flex-basis: 450px"
+    >
+      <h1 class="text-3xl text-blue font-medium mb-2">
+        Inicia sesión
+      </h1>
+      <h2 class="text-base text-black font-normal mb-6">
+        Ingresa los datos necesarios para iniciar sesión.
+      </h2>
+      <input-base
+        id="email"
+        v-model="credentials.username"
+        class="mb-3.5"
+        label="Correo:"
+        placeholder="Ingresa tu correo"
+      />
+      <input-base
+        id="password"
+        v-model="credentials.password"
+        class="mb-3.5"
+        label="Contraseña:"
+        placeholder="Ingresa tu contraseña"
+        type="password"
+      />
+      <p class="text-red text-base" v-show="error" >{{ error }}</p>
+      <button-base
+        class="max-w-xs mx-auto mt-16 sm:mr-0"
+        :loading="loading"
+        :disabled="disabled"
+        label="Ingresar"
+        @click="login"
+      />
+    </section>
+  </main>
 </template>
 
 <script>
-export default {
+import InputBase from '../components/InputBase.vue';
+import ButtonBase from '../components/ButtonBase.vue';
 
+export default {
+  name: 'LoginView',
+  components: {
+    InputBase,
+    ButtonBase,
+  },
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: '',
+      },
+      loading: false,
+      disabled: false,
+      error: '',
+    };
+  },
+  methods: {
+    login() {
+      console.log('hello');
+    },
+  },
 };
 </script>
 
