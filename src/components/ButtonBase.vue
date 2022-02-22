d548f169f2e7fcd77fd520aa2e1438e823f1c69e
vikoss <vikoss.yiss@gmail.com>
2022-02-21 20:47:04 -0600

    add button base web component

---
+++ src/components/ButtonBase.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    class="bg-blue-600 text-white block h-11 w-full font-bold text-base
+      rounded-md cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray"
+  >
+    <svg-circle
+      v-show="loading"
+    />
+    <span v-show="!loading">
+      {{ label }}
+    </span>
+  </button>
+</template>
+
+<script>
+import svgCircle from './SVG/Circle.vue';
+
+export default {
+  name: 'ButtonBase',
+  components: { svgCircle },
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
