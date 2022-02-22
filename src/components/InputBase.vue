055d923a0cd0cca5f6da5ed7ffc5a464e5050460
vikoss <vikoss.yiss@gmail.com>
2022-02-21 20:26:44 -0600

    add input base component web

---
+++ src/components/InputBase.vue
@@ -0,0 +1,56 @@
+<template>
+  <label
+    :for="id"
+    class="text-blue-600 text-base font-medium block relative"
+  >
+    {{ label }}
+    <input
+      :id="id"
+      class="h-11 py-3 px-3.5 rounded-md border border-blue-600 border-solid outline-none
+        text-black text-base font-medium mt-1 w-full focus:border-2 focus:shadow"
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      :autocomplete="autocomplete"
+      :disabled="disabled"
+      @input="(event) => $emit('update:modelValue', event.target.value)"
+    >
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'InputBase',
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    autocomplete: {
+      type: String,
+      default: 'off',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['update:modelValue'],
+};
+</script>
