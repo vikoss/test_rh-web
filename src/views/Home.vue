8bfb69086b3f1fd6518358feb34b0889c7ddc3ff
vikoss <vikoss.yiss@gmail.com>
2022-02-22 19:37:59 -0600

    add mockup to view home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,81 @@
 <template>
   <main>
     <header-base />
-    <input-base
-      id="email"
-      v-model="email"
-      class="mb-3.5"
-      label="Usuario:"
-      placeholder="Ingresa tu usuario"
-    />
     <button-base
-      class="max-w-xs mx-auto"
-      :loading="true"
-      :disabled="true"
-      label="Ingresar"
-      @click="login"
+      v-if="isAdmin"
+      class="max-w-xs mx-auto mr-5 mt-7"
+      style="background-color: white; color: #2563eb; border-color: #2563eb; border-width: 2px;"
+      label="Contratar"
+      :click="redirect"
     />
-
+    <div class="sm:p-12">
+      <h1
+        class="text-xl mb-5"
+      >
+        <strong>
+          Felicidades
+        </strong>
+        estos son los puestos de trabajo que estas desempeñando en RH
+      </h1>
+      <div v-for="job in jobs" :key="job.id">
+        <span class="h-1 bg-blue-900 block my-1"></span>
+        <div class="sm:px-11">
+          <h2>
+            Puesto:
+            <strong>{{ job.name }}</strong>
+          </h2>
+          <h2>
+            Codigo:
+            <strong>{{ job.code }}</strong>
+          </h2>
+          <h2>
+            Nivel:
+            <strong>{{ job.level }}</strong>
+          </h2>
+          <h2>
+            Eres jefe:
+            <strong>{{ job.is_boss ? 'Si': 'No' }}</strong>
+          </h2>
+          <h2>
+            Nos complace saber que estas con nosotros desde:
+            <strong>{{ new Date(job.created_at) }}</strong>
+          </h2>
+        </div>
+        <span class="h-1 bg-blue-900 block"></span>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-import InputBase from '../components/InputBase.vue';
 import HeaderBase from '../components/HeaderBase.vue';
 import ButtonBase from '../components/ButtonBase.vue';
+import { getJobs } from '../api/user';
+import { currentUser } from '../helpers/localStorage';
 
 export default {
   name: 'Home',
   components: {
-    InputBase,
     HeaderBase,
     ButtonBase,
   },
   data() {
     return {
-      email: 0,
+      jobs: [],
     };
   },
-  methods: {
-    login() {
-      console.log('helooo');
+  computed: {
+    isAdmin() {
+      return currentUser.role === 'admin';
     },
+  },
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Hire' });
+    },
+  },
+  async created() {
+    this.jobs = await getJobs();
   },
 };
 </script>
