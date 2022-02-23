a7225de60de379aa5e9780d23982bcf6a4c8fed7
vikoss <vikoss.yiss@gmail.com>
2022-02-22 19:40:32 -0600

    add middleware to all routes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
+import { JWT } from './helpers/localStorage';
 
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !JWT()) next({ name: 'Login' });
+  else next();
+});
 
 new Vue({
   router,
